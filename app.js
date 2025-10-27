08f13d905e735435707ec0228447a17dc1d66faa
shivam847 <shivsisodiya3172000@gmail.com>
2025-10-27 23:18:53 +0530

    form handling

--- app.js
+++ app.js
@@ -21,28 +21,53 @@
 
 
 // express using
+// const express=require('express')
+// const app=express()
+// app.set("view engine",'ejs')
+
+// // custom middleware
+// app.use((req,res,next)=>{
+//     console.log(5)
+//     return next()
+// })
+
+// app.get('/',(req,res)=>{
+//     // res.send('hello')
+//     res.render('index')
+// })
+
+// // custom middleware for only /about route 
+// app.get('/about',(req,res,next)=>{
+//     console.log(65)
+//     next()
+// },(req,res)=>{
+//     res.send('about myself')
+// })
+
+
+// app.listen(3000)
+
+
+
 const express=require('express')
 const app=express()
 app.set("view engine",'ejs')
 
-// custom middleware
-app.use((req,res,next)=>{
-    console.log(5)
-    return next()
-})
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
-    // res.send('hello')
     res.render('index')
 })
 
-// custom middleware for only /about route 
-app.get('/about',(req,res,next)=>{
-    console.log(65)
-    next()
-},(req,res)=>{
-    res.send('about myself')
-})
+// app.get('/get-data',(req,res)=>{
+//     console.log(req.query)
+//     res.send('data received')
+// })
 
+app.post('/get-data',(req,res)=>{
+    console.log(req.body)
+    res.send('data received')
+})
 
 app.listen(3000)
